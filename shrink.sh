612ae2a99fff38905277a0d4257ba03f368390b5
Josh <jocjohns@mtu.edu>
2016-11-22 01:55:37 -0500

    Fixed line skipping. It's late...

--- shrink.sh
+++ shrink.sh
@@ -1,1 +1,1 @@
-sed -n '0~3p' $1 | awk '/latitudeE7|longitudeE7/' | sed -e 's/\("\| \|:\|latitudeE7\|longitudeE7\|,\)//g' > locations.txt
+awk '/latitudeE7|longitudeE7/' $1 | sed -e 's/\("\| \|:\|latitudeE7\|longitudeE7\|,\)//g' | awk 'NR % 6 == 0 || NR % 6 == 1' > locations.txt
